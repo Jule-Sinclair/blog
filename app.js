42cfddddc6b6e3d1bf260f2c9c0dcb2ac43e08af
JunWoo <junu.sinclair@gmail.com>
2018-07-24 20:52:03 +0900

    add express for node

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const PORT = process.env.PORT
+const app = express();
+
+apt.get("/", (req, res) => {
+  res.send({ hello: 'world' });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(PORT);
